f1bf0f98eb0e3e77892fefc2438a7f773c1513e7
alemesh <alemesh@acceptic.com>
2018-06-20 15:29:12 +0300

    initial commit

---
+++ app/form_processing.php
@@ -0,0 +1,66 @@
+<?php
+//****************************************
+//edit here
+$senderName = 'nadine-kezev-haghoof-2';
+$senderEmail = $_SERVER['SERVER_NAME'];
+$targetEmail = [];
+$targetEmail = ['office@gofmans.co.il', 'ravit@gofmans.co.il', 'eli@gofmans.co.il', 'alemesh@acceptic.com', 'bommer@netvision.net.il'];
+//$targetEmail = ['alemesh@acceptic.com'];
+$messageSubject = 'Message from web-site - '. $_SERVER['SERVER_NAME'];
+$redirectToReferer = true;
+$redirectURL = $_SERVER['SERVER_NAME'];
+//****************************************
+
+// mail content
+
+//var_dump($_POST); die;
+$ufname = $_POST['name'];
+$uphone = $_POST['tel'];
+$umail = $_POST['email'];
+$sources = $_POST['sources-3'];
+$description = $_POST['description'];
+
+
+
+
+
+// prepare message text
+$messageText =	'Name: '.$ufname."\n".
+    'Phone: '.$uphone."\n".
+    'Email: '.$umail."\n".
+    'Description: '.$description."\n".
+    'Branch: '.$sources."\n";
+
+
+// send email
+$senderName = "=?UTF-8?B?" . base64_encode($senderName) . "?=";
+$messageSubject = "=?UTF-8?B?" . base64_encode($messageSubject) . "?=";
+$messageHeaders = "From: " . $senderName . " <" . $senderEmail . ">\r\n"
+    . "MIME-Version: 1.0" . "\r\n"
+    . "Content-type: text/plain; charset=UTF-8" . "\r\n";
+
+//if (preg_match('/^[_.0-9a-z-]+@([0-9a-z][0-9a-z-]+.)+[a-z]{2,4}$/',$targetEmail,$matches))
+foreach ($targetEmail as $val){
+    mail($val, $messageSubject, $messageText, $messageHeaders);
+}
+
+
+//========== xml backups lids ================
+
+$today = date("F j, Y, g:i a");
+
+$file = 'sample.csv';
+$tofile = "$ufname;$uphone;$umail;$sources;$description;$today\n";
+$bom = "\xEF\xBB\xBF";
+@file_put_contents($file, $bom . $tofile . file_get_contents($file));
+
+
+$redirectToTnxPage = 'http://campaign.gofmans.co.il/nadine-kezev-haghoof-2/thanks-page.html?Lead=true';
+//$redirectToTnxPage = 'http://192.168.89.147/thanks-page.html?Lead=true';
+// redirect
+if($redirectToReferer) {
+    header("Location: ".$redirectToTnxPage);
+} else {
+    header("Location: ".$redirectURL);
+}
+
---
+++ app/js/common.js
@@ -0,0 +1,117 @@
+$(function() {
+
+	// Custom JS
+
+
+    //slick slider
+
+    (function($) {
+        $('.variable-width').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+            prevArrow: '<span class="slick-prev"></span>',
+            nextArrow: '<span class="slick-next"></span>',
+            variableWidth: true,
+            autoplay: true,
+            autoplaySpeed: 3000
+        });
+    })(jQuery);
+
+
+//========= modal ==============
+    var modal = document.getElementById('myModal');
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+    btn.onclick = function(e) {
+        e.preventDefault();
+        modal.style.display = "block";
+    };
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+
+
+//================================
+
+
+
+
+});
+
+
+//========== custom select =================
+$(".custom-select").each(function() {
+    var classes = $(this).attr("class"),
+        id      = $(this).attr("id"),
+        name    = $(this).attr("name");
+    var template =  '<div class="' + classes + '">';
+    template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + '</span>';
+    template += '<div class="custom-options">';
+    $(this).find("option").each(function() {
+        template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+    });
+    template += '</div></div>';
+
+    $(this).wrap('<div class="custom-select-wrapper"></div>');
+    $(this).hide();
+    $(this).after(template);
+});
+$(".custom-option:first-of-type").hover(function() {
+    $(this).parents(".custom-options").addClass("option-hover");
+}, function() {
+    $(this).parents(".custom-options").removeClass("option-hover");
+});
+$(".custom-select-trigger").on("click", function() {
+    $('html').one('click',function() {
+        $(".custom-select").removeClass("opened");
+    });
+    $(this).parents(".custom-select").toggleClass("opened");
+    event.stopPropagation();
+});
+$(".custom-option").on("click", function() {
+    $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
+    $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
+    $(this).addClass("selection");
+    $(this).parents(".custom-select").removeClass("opened");
+    $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
+});
+//==========================================
+
+//=============================scroll on form=============================
+$(function() {
+
+    $(document).ready(function(){
+        $(".button-mobile").on("click","a", function (event) {
+            //отменяем стандартную обработку нажатия по ссылке
+            event.preventDefault();
+
+            //забираем идентификатор бока с атрибута href
+            var id  = $(this).attr('href'),
+
+                //узнаем высоту от начала страницы до блока на который ссылается якорь
+                top = $(id).offset().top;
+
+            //анимируем переход на расстояние - top за 1500 мс
+            $('body,html').animate({scrollTop: top}, 500);
+        });
+    });
+
+});
+
+//====================================================================
